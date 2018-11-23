d012bbdbd57044bf361adb06b8717d365bfb2fb0
Bret Hudson <bret@brethudson.com>
2018-11-23 01:42:26 -0600

    Creating words via JS

--- main.js
+++ main.js
@@ -3,13 +3,28 @@ const IS_TOUCH_DEVICE = !!(('ontouchstart' in window) ||
 	window.DocumentTouch && document instanceof DocumentTouch);
 
 let style = $new('style[type=text/css]').element();
-let boardElem, wordsElem;
+let boardElem, wordsHolderElem;
+
+const words = [
+	'awesome',
+	'bad',
+	'bitchin',
+	'cool',
+	'epic',
+	'gnarly',
+	'radical',
+	'righteous',
+	'sick',
+	'tubular',
+	'wicked',
+];
 
 document.on('DOMContentLoaded', (e) => {
 	document.head[0].append(style);
 	
-	boardElem = document.q('#board');
-	wordsElem = document.q('#words');
+	let wordsElem = document.body.appendChild($new('.words').element());
+	wordsHolderElem = wordsElem.appendChild($new('.words-holder').element());
+	boardElem = document.body.appendChild($new('.board').element());
 	
 	let boardSize = 15;
 	let numTiles = boardSize * boardSize;
@@ -18,24 +33,21 @@ document.on('DOMContentLoaded', (e) => {
 	boardElem.style.width = `${boardWidth}em`;
 	boardElem.style.left = `calc(50% - ${boardHalfWidth}em)`;
 	
+	wordsElem.style.paddingTop = `${boardWidth}em`;
 	wordsElem.style.width = `${boardWidth}em`;
 	wordsElem.style.left = `calc(50% - ${boardHalfWidth}em)`;
-	wordsElem.style.top = `${boardWidth}em`;
 	
-	let tileSize = boardWidth / boardSize;
+	let tileSize = (boardWidth - 3) / boardSize; // NOTE(bret): for that 1.5em padding yo
 	style.textContent = `.tile { width: ${tileSize}em; height: ${tileSize}em; }`;
 	
 	for (let t = 0; t < numTiles; ++t) {
-		let tile =
-			$new('.tile').children(
-				$new('.background'),
-				$new('.letter').attr('data-letter', 'a')
-			);
+		let tile = createTile();
 		boardElem.append(tile);
 	}
 	
 	const resize = () => {
 		let navHeight = 5;
+		let tileSize = boardWidth / boardSize;
 		let size = Math.min(
 			window.innerWidth / (boardSize + 0) / tileSize,
 			window.innerHeight / (navHeight + boardSize + 0) / tileSize
@@ -43,6 +55,21 @@ document.on('DOMContentLoaded', (e) => {
 		document.body.style.fontSize = `${size}px`;
 	};
 	
+	words.forEach(word => addWord(word));
+	
 	window.on('resize', resize);
 	resize();
 });
+
+let createTile = (letter) => {
+	return $new('.tile' + ((letter) ? '.has-letter' : '')).children(
+		$new('.background'),
+		$new('.letter').attr('data-letter', letter || '')
+	);
+};
+
+let addWord = (word) => {
+	wordsHolderElem.append(word.split('').reduce((wordElem, letter) => {
+		return wordElem.child(createTile(letter));
+	}, $new('.word')));
+};
