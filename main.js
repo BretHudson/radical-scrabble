84e8682c733676a7731b69063f505eb3e658c9b3
Bret Hudson <bret@brethudson.com>
2018-11-23 02:37:39 -0600

    Overlap code implemented

--- main.js
+++ main.js
@@ -53,6 +53,17 @@ document.on('DOMContentLoaded', (e) => {
 			dragPos.y = e.clientY;
 			
 			setWordPos(dragWord, dragPos.x - dragStartPos.x, dragPos.y - dragStartPos.y);
+			
+			let x, y;
+			boardElem.q('.tile').each(tile => {
+				tile.removeClass('test');
+				dragWord.q('.tile').each(letter => {
+					x = letter.center.x + dragPos.x;
+					y = letter.center.y + dragPos.y;
+					if (overlapTile(tile, x, y))
+						tile.addClass('test');
+				});
+			});
 		}
 	});
 	
@@ -62,6 +73,7 @@ document.on('DOMContentLoaded', (e) => {
 	});
 	
 	const resize = () => {
+		// Resize the board
 		let navHeight = 5;
 		let tileSize = boardWidth / boardSize;
 		let size = Math.min(
@@ -69,6 +81,13 @@ document.on('DOMContentLoaded', (e) => {
 			window.innerHeight / (navHeight + boardSize + 0) / tileSize
 		);
 		document.body.style.fontSize = `${size}px`;
+		
+		// Update each tile's position
+		window.requestAnimationFrame(() => {
+			boardElem.q('.tile').each(tile => {
+				tile.pos = tile.getBoundingClientRect();
+			});
+		});
 	};
 	
 	words.forEach(word => addWord(word));
@@ -84,23 +103,37 @@ let createTile = (letter) => {
 	);
 };
 
+let overlapTile = (tile, x, y) => {
+	return (x >= tile.pos.left) && (x < tile.pos.right) && (y >= tile.pos.top) && (y < tile.pos.bottom)
+};
+
 let setWordPos = (word, x, y, before = null) => {
 	window.requestAnimationFrame(() => {
 		if (before !== null) before();
-		word.style.left = dragWordPos.x + x + 'px';
-		word.style.top = dragWordPos.y + y + 'px';
+		word.style.left = word.pos.x + x + 'px';
+		word.style.top = word.pos.y + y + 'px';
 	});
 };
 
-let dragWordPos = { x: 0, y: 0 };
 let dragStartPos = { x: 0, y: 0 };
 let dragPos = { x: 0, y: 0 };
-let dragWord;
-let beginWordDrag = (word) => {
+let dragWord = null;
+let beginWordDrag = (word, mx, my) => {
 	console.log('begin drag');
+	
+	dragStartPos.x = dragPos.x = mx;
+	dragStartPos.y = dragPos.y = my;
+	
 	let rect = word.getBoundingClientRect();
-	dragWordPos.x = rect.left;
-	dragWordPos.y = rect.top;
+	word.pos = { x: rect.x, y: rect.y };
+	word.q('.tile').each(tile => {
+		rect = tile.getBoundingClientRect();
+		tile.center = {
+			x: rect.x + (rect.width / 2) - mx,
+			y: rect.y + (rect.height / 2) - my
+		};
+		console.log(tile.center);
+	});
 	window.requestAnimationFrame(() => {
 		setWordPos(word, 0, 0, () => {
 			document.body.append(word);
@@ -121,9 +154,7 @@ let addWord = (word) => {
 	let wordElem =
 		$new('.word')
 			.on('mousedown', function(e) {
-				dragStartPos.x = dragPos.x = e.clientX;
-				dragStartPos.y = dragPos.y = e.clientY;
-				beginWordDrag(this);
+				beginWordDrag(this, e.clientX, e.clientY);
 			});
 	wordsHolderElem.append(word.split('').reduce((wordElem, letter) => {
 		return wordElem.child(createTile(letter));
